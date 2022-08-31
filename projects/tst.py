3b7048f9feec29b6217debcd949a1632436386a4
Sinho <choi_sinho@naver.com>
2022-08-31 17:58:03 +0900

    0831

---
+++ projects/tst.py
@@ -0,0 +1,27 @@
+data = {'kor':90, 'eng':99, 'name':'hong'}
+print(data.keys())
+for k in data.keys():
+    print(data[k])
+# az = b'AtoZ'
+# heart = bytes(b'\xe2\x99\xa5')
+# print(az)
+# print(heart)
+# print(heart.decode())
+# print(az[1])
+# print(heart[1], hex(heart[1]))
+# data = list(range(0, 5))
+# data1 = list(range(11, 15))
+# data.extend(data1)
+# print(data)
+# for v in data[::-1]: #역순
+#     print(v)
+# print(data)
+# print(data[::])
+# print(data[::-1])
+# for v in data:
+#     if v % 2 == 0:
+#         print("even")
+#     else:
+#         print('odd')
+# else:
+#     print('end of for')
